ad828e3274d1efe0cc05fbc573ca9c3a2c676bdb
Giovanni Campagna <gcampagna@src.gnome.org>
2013-12-30 14:50:56 +0100

    core: always set up kdbus, even when a bus already exists
    If we're running as the user manager, the system manager set
    a DBUS_SESSION_BUS_ADDRESS in the environment for legacy services,
    but that should not stop us from setting a bus, otherwise it
    points to nothing.

--- src/core/manager.c
+++ src/core/manager.c
@@ -538,10 +538,6 @@ static int manager_setup_kdbus(Manager *m) {
         if (m->kdbus_fd >= 0)
                 return 0;
 
-        /* If there's already a bus address set, don't set up kdbus */
-        if (m->running_as == SYSTEMD_USER && getenv("DBUS_SESSION_BUS_ADDRESS"))
-                return 0;
-
         m->kdbus_fd = bus_kernel_create_bus(m->running_as == SYSTEMD_SYSTEM ? "system" : "user", m->running_as == SYSTEMD_SYSTEM, &p);
         if (m->kdbus_fd < 0) {
                 log_debug("Failed to set up kdbus: %s", strerror(-m->kdbus_fd));
